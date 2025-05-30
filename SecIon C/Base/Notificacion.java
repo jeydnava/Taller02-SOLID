92e2a2069b331f36fa18797cfc1a722be0c6bddf
Mateo Andrade <matanand@espol.edu.ec>
2025-05-29 22:55:18 -0500

    seccion b

---
+++ SecIon C/Base/Notificacion.java
@@ -0,0 +1,13 @@
+package Base;
+
+public class Notificacion {
+    private int tipoNotificacion;
+
+    public void notificar(Pago pago) {
+        if (tipoNotificacion == 1) {
+            // enviarEmail();
+        } else {
+            // enviarSMS();
+        }
+    }
+}
---
+++ SecIon C/Base/Pago.java
@@ -0,0 +1,17 @@
+package Base;
+
+public class Pago {
+    protected double monto;
+
+    public void realizarCobro(double monto) {
+        // cargar el monto de compra al medio de pago
+    }
+
+    public void calcularImpuestosFactura() {
+        // calcula los impuestos asociados a la compra
+    }
+
+    public void generarFactura() {
+        // generar una nueva factura
+    }
+}
